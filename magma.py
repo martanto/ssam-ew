513e1fa6780077cf69baccc0b884a97edecfba9d
Martanto <martanto@live.com>
2024-05-04 21:21:56 +0200

    Update magma.py

--- magma.py
+++ magma.py
@@ -1,6 +1,7 @@
 from typing import Tuple
-from datetime import datetime
+from datetime import datetime, timedelta
 from pandas.errors import EmptyDataError
+from pathlib import Path
 
 import os
 import json
@@ -8,8 +9,6 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from pathlib import Path
-
 
 colors: dict[str, str] = {
     'Letusan/Erupsi': '#F44336',
@@ -56,37 +55,128 @@ columns: dict[str, str] = {
     'gempa.tremor_menerus': 'Tremor Menerus'
 }
 
-class Magma:
-    url = 'https://magma.esdm.go.id/api/v1/python/magma-var/evaluasi'
 
-    def __init__(self, token: str, volcano_code: str, start_date: str, end_date: str,
-                 earthquake_events: str | list[str] = None) -> None:
+class Auth:
+    url_login = 'https://magma.esdm.go.id/api/login'
+    url_validate_token = 'https://magma.esdm.go.id/api/status'
 
-        self.volcano_code: str = volcano_code
-        self.start_date: str = start_date
-        self.end_date: str = end_date
-        self.output_dir, self.figures_dir = self.check_directory(os.getcwd())
+    def __init__(self, token: str = None, username: str = None, password: str = None) -> None:
 
-        self.earthquake_events: list[str] = self.validate_earthquake_events(earthquake_events)
+        self.token: str = token
+        self.expired = None
+        self.success: bool = False
+        self.df: pd.DataFrame = pd.DataFrame()
 
-        self.json: dict = self.get_json_response(token)
-        self.df: pd.DataFrame = self.get_df()
-        self.events_not_recorded = self.df.columns[self.df.sum()==0]
-        self.filename = 'magma_{}_{}_{}'.format(volcano_code, start_date, end_date)
-
-        if not self.df.empty:
-            csv = os.path.join(self.output_dir, "{}.csv".format(self.filename))
-            self.df.to_csv(csv)
-            print(f'💾 Saved to {csv}')
+        if (token is None) and ((username is not None) and (password is not None)):
+            self.token = self.login(username, password)
+            self.expired = datetime.now() + timedelta(hours=24)
         else:
-            print('⚠️ There is no event(s) between {} and {}. Please change your parameters.'.format(start_date, end_date))
-            raise EmptyDataError
+            if self.validate_token():
+                self.success = True
+            if self.success:
+                print('✅ Authenticated. Selamat datang di API MAGMA Indonesia')
 
-    def get_df(self, json: dict = None) -> pd.DataFrame:
-        if json is None:
-            json = self.json
+    def validate_token(self) -> bool:
+        headers = {'Authorization': 'Bearer ' + self.token}
 
-        df = pd.json_normalize(json['data'])
+        try:
+            response = requests.request("GET", self.url_validate_token, headers=headers).json()
+            self.expired = datetime.utcfromtimestamp(response['exp'])
+        except Exception as e:
+            raise f'Error validating token: {e}'
+
+        if 'code' in response:
+            if response['code'] == 419:
+                return False
+
+        return True
+
+    def login(self, username: str, password: str) -> str:
+        payload = json.dumps({
+            "username": username,
+            "password": password
+        })
+
+        headers = {'Content-Type': 'application/json'}
+
+        try:
+            response = requests.request("POST", self.url_login, headers=headers, data=payload).json()
+        except Exception as e:
+            raise f'Error login with username and password: {e}'
+
+        if not response['success']:
+            raise ValueError(f'Wrong username or password')
+
+        return response['token']
+
+
+class Plot(Auth):
+    url_evaluasi = 'https://magma.esdm.go.id/api/v1/python/magma-var/evaluasi'
+
+    def __init__(self, token: str, volcano_code: str = None, start_date: str = None, end_date: str = None,
+                 earthquake_events: str | list[str] = None, current_dir: str = None) -> None:
+
+        super().__init__(token)
+        self.volcano_code = volcano_code
+        self.start_date = start_date
+        self.end_date = end_date
+        self.earthquake_events: list[str] = Plot.validate_earthquake_events(earthquake_events)
+
+        self.json: dict = self.get_json_response()
+        self.df: pd.DataFrame = self.get_df()
+        self.events_not_recorded = self.df.columns[self.df.sum() == 0]
+        self.filename = 'magma_{}_{}_{}'.format(self.volcano_code, self.start_date, self.end_date)
+
+        self.current_dir = current_dir
+        if self.current_dir is None:
+            self.current_dir = os.getcwd()
+
+        self.output_dir, self.figures_dir = self.check_directory()
+
+        if start_date is None:
+            self.start_date: str = datetime.today().strftime('%Y-%m-%d')
+
+        if end_date is None:
+            self.end_date = (datetime.today() + timedelta(days=30)).strftime('%Y-%m-%d')
+
+        self.csv = self.download()
+
+    def check_directory(self, current_dir: str = None) -> Tuple[str, str]:
+
+        if current_dir is None:
+            current_dir = self.current_dir
+
+        output_dir = os.path.join(current_dir, 'output')
+        os.makedirs(output_dir, exist_ok=True)
+
+        figures_dir = os.path.join(current_dir, 'figures')
+        os.makedirs(figures_dir, exist_ok=True)
+
+        return output_dir, figures_dir
+
+    @staticmethod
+    def validate_earthquake_events(earthquake_events: str | list[str] = None) -> list[str]:
+        if earthquake_events is None:
+            return ['*']
+
+        if isinstance(earthquake_events, str):
+            earthquake_events = [earthquake_events]
+
+        for earthquake_event in earthquake_events:
+            if earthquake_event not in ['*', 'lts', 'apl', 'apg', 'gug', 'hbs', 'hrm', 'tre',
+                                        'tor', 'lof', 'hyb', 'vtb', 'vta', 'vlp', 'tel', 'trs',
+                                        'tej', 'dev', 'gtb', 'dpt', 'mtr']:
+                raise ValueError("Earthquake_events must be one of '*', 'lts', 'apl', 'apg', 'gug', 'hbs', 'hrm', "
+                                 "'tre', 'tor', 'lof', 'hyb', 'vtb', 'vta','vlp', 'tel', 'trs', 'tej', 'dev', 'gtb', "
+                                 "'dpt', 'mtr'")
+
+        return earthquake_events
+
+    def get_df(self, json_response: dict = None) -> pd.DataFrame:
+        if json_response is None:
+            json_response = self.json
+
+        df = pd.json_normalize(json_response['data'])
         df.drop(columns=[
             'availability',
             'visual.visibility',
@@ -117,8 +207,12 @@ class Magma:
 
         return df
 
-    def get_json_response(self, token: str) -> dict:
-        url = self.url
+    def get_json_response(self, token: str = None) -> dict:
+
+        if token is None:
+            token = self.token
+
+        url = self.url_evaluasi
 
         payload = json.dumps({
             "start_date": self.start_date,
@@ -136,7 +230,8 @@ class Magma:
         end_date_object = datetime.strptime(self.end_date, "%Y-%m-%d")
 
         if start_date_object > end_date_object:
-            raise ValueError('End date ({}) must be greater than start date ({})'.format(self.end_date, self.start_date))
+            raise ValueError(
+                'End date ({}) must be greater than start date ({})'.format(self.end_date, self.start_date))
 
         if (end_date_object > datetime.now()) or (start_date_object > datetime.now()):
             raise ValueError('End date or start date must be greter than today ({})'.format(datetime.today().date()))
@@ -152,34 +247,101 @@ class Magma:
 
         return response
 
-    def validate_earthquake_events(self, earthquake_events: str | list[str] = None) -> list[str]:
-        if earthquake_events is None:
-            earthquake_events = ['*']
+    def download(self) -> str:
+        """Download daily seismic from MAGMA Indonesia
 
-        if isinstance(earthquake_events, str):
-            earthquake_events = [earthquake_events]
+        Returns:
+            str: path to downloaded file
+        """
+        if not self.df.empty:
+            try:
+                csv = os.path.join(self.output_dir, "{}.csv".format(self.filename))
+                self.df.to_csv(csv)
+                self.csv = csv
+                print(f'💾 Saved to {csv}')
 
-        for earthquake_event in earthquake_events:
-            if earthquake_event not in ['*', 'lts', 'apl', 'apg', 'gug', 'hbs', 'hrm', 'tre',
-                                         'tor', 'lof', 'hyb', 'vtb', 'vta', 'vlp', 'tel', 'trs',
-                                         'tej', 'dev', 'gtb', 'dpt', 'mtr']:
-                raise ValueError("Earthquake_events must be one of '*', 'lts', 'apl', 'apg', 'gug', 'hbs', 'hrm', "
-                                 "'tre', 'tor', 'lof', 'hyb', 'vtb', 'vta','vlp', 'tel', 'trs', 'tej', 'dev', 'gtb', "
-                                 "'dpt', 'mtr'")
-
-        return earthquake_events
-
-    def check_directory(self, current_dir: str) -> Tuple[str, str]:
-        output_dir = os.path.join(current_dir, 'output')
-        os.makedirs(output_dir, exist_ok=True)
-
-        figures_dir = os.path.join(current_dir, 'figures')
-        os.makedirs(figures_dir, exist_ok=True)
-
-        return output_dir, figures_dir
+                return csv
+            except Exception as e:
+                raise FileNotFoundError(f'Failed to save csv file: {e}')
+        else:
+            print('⚠️ There is no event(s) between {} and {}. '
+                  'Please change your parameters.'.format(self.start_date, self.end_date))
+            raise EmptyDataError
 
     @staticmethod
-    def plot_ax(ax: plt.Axes, df: pd.DataFrame, column_name: str, width: float = 0.5, interval = 1) -> plt.Axes:
+    def from_csv(csv: str, interval: int = 1, width: float = 0.5, save_plot: bool = True,
+                 title: str = None, dpi: int = 300) -> Tuple[plt.Figure, plt.Axes]:
+        """Plot from csv file.
+
+        Args:
+            csv (str): path to csv file
+            interval (int, optional): Xtick label interval (day). Defaults to 1.
+            width (float, optional): Width of column bar. Defaults to 0.5.
+            save_plot (bool, optional): Save plot. Defaults to True.
+            title (str, optional): Title of plot. Defaults to 'Kegempaan'.
+            dpi (int, optional): Image resolution. Defaults to 300.
+
+        Returns:
+            Tuple[plt.Figure, plt.Axes]: Figure, Axes
+        """
+
+        if title is None:
+            title = 'Kegempaan'
+
+        filename = Path(csv).stem
+
+        df = pd.read_csv(csv, index_col='date', parse_dates=True)
+
+        fig, axs = plt.subplots(nrows=len(df.columns), ncols=1, figsize=(12, 1 * len(df.columns)), sharex=True)
+
+        plt.subplots_adjust(hspace=0.0)
+
+        for gempa, column_name in enumerate(df.columns):
+            Plot.ax(
+                ax=axs[gempa],
+                df=df,
+                column_name=column_name,
+                width=width,
+                interval=interval
+            )
+
+        fig.supylabel('Jumlah', x=0.07)
+        fig.suptitle(title, fontsize=12, y=0.92)
+
+        if save_plot:
+            figures_dir = os.path.join(os.getcwd(), 'figures')
+            os.makedirs(figures_dir, exist_ok=True)
+
+            figure_name = os.path.join(figures_dir, f'{filename}.png')
+            fig.savefig(figure_name, dpi=dpi)
+
+        return fig, axs
+
+    def show(self, interval: int = 1, width: float = 0.5, save_plot: bool = True,
+             title: str = None, dpi: int = 300) -> Tuple[plt.Figure, plt.Axes]:
+        """Show plot in matplotlib
+
+        Args:
+            interval (int, optional): Xtick label interval (day). Defaults to 1.
+            width (float, optional): Width of column bar. Defaults to 0.5.
+            save_plot (bool, optional): Save plot. Defaults to True.
+            title (str, optional): Title of plot. Defaults to 'Kegempaan'.
+            dpi (int, optional): Image resolution. Defaults to 300.
+
+        Returns:
+            Tuple[plt.Figure, plt.Axes]: Figure, Axes
+        """
+        return Plot.from_csv(
+            csv=self.csv,
+            interval=interval,
+            width=width,
+            save_plot=save_plot,
+            title=title,
+            dpi=dpi
+        )
+
+    @staticmethod
+    def ax(ax: plt.Axes, df: pd.DataFrame, column_name: str, width: float = 0.5, interval=1) -> plt.Axes:
 
         ax.bar(df.index, df[column_name], width=width, label=column_name,
                color=colors[column_name], linewidth=0)
@@ -207,65 +369,3 @@ class Magma:
         #     axs[gempa].axvline(datetime.strptime(date, '%Y-%m-%d'),
         #                        color='red', label="_" * key + 'Single Eruption')
         return ax
-
-    def plot(self, width: float = 0.5, interval: int = 1, save_plot: bool = True, dpi: int = 300) -> plt.Figure:
-        df = self.df
-        fig, axs = plt.subplots(nrows=len(df.columns), ncols=1, figsize=(12, 1 * len(df.columns)), sharex=True)
-
-        plt.subplots_adjust(hspace=0.0)
-
-        for gempa, column_name in enumerate(df.columns):
-            Magma.plot_ax(
-                ax=axs[gempa],
-                df=df,
-                column_name=column_name,
-                width=width,
-                interval=interval
-            )
-
-        fig.supylabel('Jumlah', x=0.07)
-        fig.suptitle('Kegempaan', fontsize=12, y=0.9)
-
-        if save_plot:
-            figure_name = os.path.join(self.figures_dir, f'{self.filename}.png')
-            fig.savefig(figure_name, dpi=dpi)
-
-        return fig
-
-    @staticmethod
-    def plot_from_csv(csv: str, width: float = 0.5, interval: int = 1, save_plot: bool = True, dpi: int = 300) -> plt.Figure:
-        filename = Path(csv).stem
-
-        df = pd.read_csv(csv, index_col='date', parse_dates=True)
-
-        fig, axs = plt.subplots(nrows=len(df.columns), ncols=1, figsize=(12, 1 * len(df.columns)), sharex=True)
-
-        plt.subplots_adjust(hspace=0.0)
-
-        for gempa, column_name in enumerate(df.columns):
-            Magma.plot_ax(
-                ax = axs[gempa],
-                df = df,
-                column_name = column_name,
-                width = width,
-                interval = interval
-            )
-
-        fig.supylabel('Jumlah', x=0.07)
-        fig.suptitle('Kegempaan', fontsize=12, y=0.9)
-
-        if save_plot:
-            figures_dir = os.path.join(os.getcwd(), 'figures')
-            os.makedirs(figures_dir, exist_ok=True)
-
-            figure_name = os.path.join(figures_dir, f'{filename}.png')
-            fig.savefig(figure_name, dpi=dpi)
-
-        return fig
-
-
-
-
-
-
-
